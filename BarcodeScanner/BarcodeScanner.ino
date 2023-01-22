08330792583a321ad68aee6d55b92e25726bb479
Scott F <98131933+SF-11@users.noreply.github.com>
2023-01-22 18:42:15 -0500

    Single barcode scan command implemented

--- BarcodeScanner/BarcodeScanner.ino
+++ BarcodeScanner/BarcodeScanner.ino
@@ -8,26 +8,69 @@
 #include <HTTPClient.h>
 #include "conf.h"
 
+#define ACK 0x06
+#define NAK 0x15
 
 WiFiMulti wifiMulti;
 
 void setup() {
-    Serial.begin(115200);
-    wifiMulti.addAP(SSID, WIFI_PASS);
+  Serial.begin(115200);
+  wifiMulti.addAP(SSID, WIFI_PASS);
+
+  int response = 0;
+  do { 
+
+    response = start_scan();
+  } while (response != ACK);
 }
 
 
 void loop() {
-    if((wifiMulti.run() == WL_CONNECTED)) {
-        HTTPClient http;
-        http.begin("http://" SERVER_IP ":8080/barcode"); //HTTP
-        http.addHeader("Content-Type", "text/plain");
-        
-        int httpCode = http.POST("715515210119");
-        String payload = http.getString();
+  char* barcode = NULL;
 
-        Serial.println(String(httpCode) + ": " + payload);
-        http.end();
-    }
-    delay(5000);
+  if (Serial.available() > 0) {
+    barcode = read_barcode();
+    post_barcode(barcode);
+  }
+  delay(5000);
+}
+
+
+void post_barcode(char* barcode) {
+  if((wifiMulti.run() == WL_CONNECTED)) {
+    HTTPClient http;
+    http.begin("http://" SERVER_IP ":8080/barcode"); //HTTP
+    http.addHeader("Content-Type", "text/plain");
+    
+    int httpCode = http.POST(barcode);
+    String payload = http.getString();
+
+    http.end();
+  }
+}
+
+
+char* read_barcode() {
+
+  char* buf = (char *) malloc(14);
+  int i = 0;
+  while (Serial.available() > 0 && i < 13) {
+    buf[i] = Serial.read();
+    i++;
+  }
+  buf[13] = '\0';
+    
+  return buf;
+}
+
+
+int start_scan() {
+  int response;
+
+  Serial.println("^_^SCAN.");
+  delay(500); 
+  if (Serial.available() > 0) {
+    response = Serial.read();
+  }
+  return response;
 }
